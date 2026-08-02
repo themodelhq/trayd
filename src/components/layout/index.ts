926da71dc28218bc0e41bcd4fdd0318789cc9c08
themodelhq <themodelhq@gmail.com>
2026-08-02 14:37:42 +0100

    Delete src directory

--- src/components/layout/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Tray'd Layout Components
- * @description Export all layout components
- */
-
-export { ThemeProvider, ThemeToggle } from './ThemeProvider';
-export { Sidebar } from './Sidebar';
-export { Header } from './Header';
-export { AppShell } from './AppShell';
